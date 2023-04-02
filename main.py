121ed57997d225ddb23748352443473c01cddb3e
Zero-i00 <clean.shiro@gmail.com>
2023-04-02 16:34:21 +0500

    add struct

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 
+def plus(a: int, b: int) -> int:
+    return a + b
 
-num1 = int(input("Введите число 1:"))
-num2 = int(input("Введите число 2:"))
-print(num1 + num2)
+
+def main():
+    num1 = int(input("Введите число 1:"))
+    num2 = int(input("Введите число 2:"))
+    print(plus(num1, num2))
+
+
+if __name__ == "__main__":
+    main()
